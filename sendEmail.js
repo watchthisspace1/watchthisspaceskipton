0a251803c8ac6b20fa42b2e9b21574175c47676b
watchthisspace1 <watchthisspaceskipton@gmail.com>
2022-03-30 20:11:14 +0000

    new contact form

--- sendEmail.js
+++ sendEmail.js
@@ -1,16 +1,8 @@
 function sendMail(contactForm) {
-    emailjs.send("service_thkukb4", "template_biu49gs", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+    emailjs.sendForm("service_thkukb4", "template_biu49gs", '#contactForm')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
 }
